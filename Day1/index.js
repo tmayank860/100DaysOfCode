e6392935289116477ecc2713544bd4bbee2355fb
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-12-18 21:54:07 +0530

    Day 2

--- Day1/index.js
+++ Day1/index.js
@@ -1,17 +1,33 @@
-console.log("Hello world");
-
+let allProducts = [];
 //api call in javascript 
 const fetchProducts = async () => {
   const response = await fetch('https://fakestoreapi.com/products');
   const products = await response.json(); 
-  return products;
+  allProducts = products;
+  renderProducts(allProducts);
 }
-
-const renderProducts = async () => {
-  const productsList = await fetchProducts();
+const search = document.getElementById('search-input');
+search.addEventListener("keyup",async function() {
+  const searchText = document.getElementById('search-input').value.toLowerCase();
+  const container = document.getElementById('container');
+  const product = document.querySelectorAll('.main-div');
+  const pname = container.getElementsByTagName('h3');
+  console.log("pname", pname, "product", product);
+   for (let i = 0; i < pname.length; i++) {
+    let match = product[i].getElementsByTagName('h3')[0];
+    if(match){
+      let textvalue = match.textContent || match.innerHTML
+      if(textvalue.toLowerCase().indexOf(searchText)> -1){
+        product[i].style.display ="";
+      }else{
+        product[i].style.display ="none";
+      }
+    }
+   }
+});
+const renderProducts = (productsList, searchText = null) => {
   console.log("productsList", productsList);
   const container = document.getElementById("container");
-  
   productsList.forEach((product) => {
     const mainDiv = document.createElement("div");
   mainDiv.classList.add("main-div");
@@ -24,7 +40,7 @@ const renderProducts = async () => {
     textBlock.classList.add("textBlock");
     const title = document.createElement("h3");
     title.textContent = product.title.length > 50 ?`${product.title.substring(0,50)}.... ` : product.title ;
-    const price = document.createElement("h3");
+    const price = document.createElement("h2");
     price.textContent = `$${product.price}`;
     textBlock.append(title);
     textBlock.append(price);
@@ -36,7 +52,7 @@ const renderProducts = async () => {
   
 };
 
-renderProducts();
+fetchProducts();
 
 
 
