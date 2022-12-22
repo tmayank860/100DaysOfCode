fe0dbd2df3d07d0dfb760c414320717a7e9e9544
tmayank860 <tmayank860@gmail.com>
2022-12-23 00:44:49 +0530

    Day 6

--- Shop Search/index.js
+++ Shop Search/index.js
@@ -1,9 +1,14 @@
 let allProducts = [], buttons = [];
+// const main=document.getElementById("main");
+// var loader=document.createElement("div");
+// loader.className="loader";
+// main.append(loader);
 //api call in javascript 
 const fetchProducts = async () => {
   const response = await fetch('https://fakestoreapi.com/products');
   const products = await response.json(); 
   allProducts = products;
+ 
   renderProducts(allProducts);
 }
 
@@ -14,7 +19,6 @@ search.addEventListener("keyup",async function() {
   const container = document.getElementById('container');
   const product = document.querySelectorAll('.main-div');
   const pname = container.getElementsByTagName('h3');
-  console.log("pname", pname, "product", product);
    for (let i = 0; i < pname.length; i++) {
     let match = product[i].getElementsByTagName('h3')[0];
     if(match){
@@ -32,30 +36,42 @@ search.addEventListener("keyup",async function() {
 // Add to cart
 const getButtons=()=>{
   buttons = document.getElementsByClassName("add-to-cart-btn");
-  console.log("buttons", buttons);
 const addToCart = (e)=>{
   const id=e.target.id;
-  console.log("id", id, allProducts[id]);
   const cartSize = document.getElementById(id).value;
   const cartElement = {
     id: id,
     cartSize:cartSize
   }
   localStorage.setItem(id, JSON.stringify(cartElement));
-  console.log(localStorage);
-  console.log("cart", cartSize);
+  // renderProducts(allProducts);
+  location.reload();
 }
 for(let button of buttons){
   button.addEventListener("click", addToCart);
 }
 }
 
+// Remove item from cart
+const getCross = ()=>{
+  const removeFromCart = (e) =>{
+    const id=e.target.id;
+    console.log(id,localStorage);
+    localStorage.removeItem(id);
+    console.log(localStorage);
+    location.reload();
+  }
+  const crossbtn = document.getElementsByClassName("crossDiv");
+  // console.log(crossbtn);
+  for(let cross of crossbtn){
+    cross.addEventListener("click", removeFromCart);
+  }
+}
 
 
 
 
-const renderProducts = (productsList, searchText = null) => {
-  console.log("productsList", productsList);
+const renderProducts = (productsList) => {
   const container = document.getElementById("container");
   productsList.forEach((product) => {
     const mainDiv = document.createElement("div");
@@ -67,7 +83,8 @@ const renderProducts = (productsList, searchText = null) => {
     btnDiv.classList.add("btn-div");
     const count = document.createElement('input');
     count.setAttribute("type","number");
-    count.setAttribute("value",1);
+    const isItemInCart = JSON.parse(localStorage.getItem(product.id));
+    count.setAttribute("value",localStorage.hasOwnProperty(product.id)? isItemInCart?.cartSize : 1);
     count.setAttribute("id", product.id);
     count.classList.add("input-box");
     const addToCartButton = document.createElement("button");
@@ -77,12 +94,19 @@ const renderProducts = (productsList, searchText = null) => {
     image.classList.add("img");
     image.src = product.image;
     const textBlock = document.createElement("div");
+    const crossDiv = document.createElement("div");
+    crossDiv.classList.add("crossDiv");
     textBlock.classList.add("textBlock");
     const title = document.createElement("h3");
     title.textContent = product.title.length > 50 ?`${product.title.substring(0,50)}.... ` : product.title ;
     const price = document.createElement("h2");
     price.textContent = `$${product.price}`;
-    addToCartButton.textContent="Add to cart"
+    addToCartButton.textContent="Add to cart";
+    const removeFromCart = document.createElement('h2');
+    removeFromCart.classList.add("remove-item");
+    removeFromCart.textContent="X";
+    removeFromCart.setAttribute('id', product.id);
+    crossDiv.setAttribute('id', product.id);
     textBlock.append(title);
     textBlock.append(price);
     imgDiv.append(image);
@@ -90,13 +114,20 @@ const renderProducts = (productsList, searchText = null) => {
     mainDiv.append(textBlock);
     btnDiv.append(count);
     btnDiv.append(addToCartButton);
+    crossDiv.append(removeFromCart);
+    localStorage.hasOwnProperty(product.id) ? btnDiv.append(crossDiv) : btnDiv.remove(crossDiv);
     mainDiv.append(btnDiv);
     container.append(mainDiv);
   });
 getButtons();
+getCross();
   
 };
 
+
+// setTimeout(() => {
+//   main.remove(loader); 
+// }, 2000);
 fetchProducts();
 
 
